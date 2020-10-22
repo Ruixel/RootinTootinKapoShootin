f91fc94cd130a019369eff3ae06b2bfcd07621bb
niallsiow@yahoo.co.uk <niallsiow@yahoo.co.uk>
2020-10-22 21:50:38 +0100

    basic moving enemy

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    float speed = 1;
+
+    float patrolTime = 1.5f;
+    float currentPatrolTime = 0;
+
+    float xInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xInput = speed * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentPatrolTime > 0)
+        {
+            currentPatrolTime = currentPatrolTime - Time.deltaTime;
+        }
+        else if (currentPatrolTime <= 0)
+        {
+            xInput  = xInput * -1;
+            currentPatrolTime = patrolTime;
+        }
+
+        transform.Translate(xInput, 0, 0);
+    }
+}
