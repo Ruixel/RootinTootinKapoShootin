f119fbcc41314132affc5b5517cb689482246702
niallsiow@yahoo.co.uk <niallsiow@yahoo.co.uk>
2020-10-23 00:03:43 +0100

    fix merge conflict

---
+++ Assets/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+
+    float smoothSpeed = 0.125f;
+    Vector3 offset = new Vector3(10, 10, 10);
+
+    void LateUpdate()
+    {
+        transform.position = player.position + offset;
+    }
+}
