2a3f0c9ffde7557255bbac59c2ec11d8b42aaf0d
niallsiow@yahoo.co.uk <niallsiow@yahoo.co.uk>
2020-10-23 11:32:02 +0100

    updated placeholder models for enemies, added a new flying enemy type

---
+++ Assets/Enemy1Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1Controller : MonoBehaviour
+{
+
+    public GameObject player;
+
+    float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 displacementFromTarget = player.transform.position - transform.position;
+        Vector3 directionToTarget = displacementFromTarget.normalized;
+        Vector3 velocity = directionToTarget * speed * Time.deltaTime;
+
+        transform.Translate(velocity);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Bullet")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/PatrollingEnemyController.cs
+++ Assets/PatrollingEnemyController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class PatrollingEnemyController : MonoBehaviour
 {
 
     float speed = 1;
