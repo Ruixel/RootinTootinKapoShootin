ba889d44f224773d9bc8bd5e35ed4a4b87d33675
niallsiow@yahoo.co.uk <niallsiow@yahoo.co.uk>
2020-10-24 22:39:02 +0100

    new enemy

---
+++ Assets/SludgeEnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SludgeEnemyController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    float timeBetweenJumps = 1.5f;
+    float timeSinceJump = 0f;
+
+    float jumpHeight = 7f;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Time.time > timeSinceJump) {
+
+            timeSinceJump = Time.time + timeBetweenJumps;
+            float randomMove = Random.Range(-5, 5);
+            rb.AddForce(new Vector2(randomMove, jumpHeight), ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/SpawnerScripts/SludgeSpawnerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SludgeSpawnerScript : MonoBehaviour
+{
+    float timeBetweenSpawn = 30f;
+    float timeSinceSpawn = 0f;
+    int numberToSpawn = 20;
+
+
+    public GameObject sludgeEnemyPrefab;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > timeSinceSpawn)
+        {
+            timeSinceSpawn = Time.time + timeBetweenSpawn;
+            for (int i = 0; i < numberToSpawn; i++)
+            {
+                GameObject sludgeEnemy = Instantiate(sludgeEnemyPrefab, transform.position, Quaternion.identity);
+                sludgeEnemy.GetComponent<SludgeEnemyController>().player = player;
+            }
+        }
+    }
+}
